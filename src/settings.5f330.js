2ff3ca04ae4611219862677543c0f5a9b3b13bd3
ngoquoctruong <ngoquocturong302@gmail.com>
2021-10-26 11:33:23 +0700

    “update”

--- src/settings.5f330.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/MainScene.fire",orientation:"",jsList:["assets/scripts/analytic/gta-amplitude.f658d.js"],bundleVers:{resources:"76450",internal:"69b9a",main:"cfc06"}};
